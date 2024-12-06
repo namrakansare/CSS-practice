a1bdbd0c32d46758ccafbd136b1fe33e9850fb6c
namrakansare <namrakansare@gmail.com>
2024-12-06 18:22:51 -0500

    keyevents in js

--- prac4.js
+++ prac4.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
     $("div").last().css("background-color", "red");
     $("p").eq(1).css("background-color", "yellow");
     $("p").filter(".intro").css("background-color", "purple");
+    $("#name").blur(function(){
+        alert("This input field has lost its focus.");
+      });
+      $("#name").change(function(){
+        alert("This input field has changed its content.");
+      });
+      $("#para1").click(function(){
+        alert("The paragraph was clicked.");
+      });
+      $("#name").keydown(function(){
+        $("#name").css("background-color", "yellow");
+      });
+      $("#name").keyup(function(){
+        $("#name").css("background-color", "pink");
+      });
 });
